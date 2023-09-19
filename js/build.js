7a51f59f9771740e16ab69377b9aff2af9eccd1a
Elizaveta Guseva <elizaveta.guseva@glovoapp.com>
2023-09-19 21:35:51 +0200

    feat:it is movig

--- js/build.js
+++ js/build.js
@@ -1,14 +1,30 @@
-var body = document.querySelector('body');
-var sketch = function (p) {
-    var x = p.windowWidth / 2;
-    var y = p.windowHeight / 2;
-    p.setup = function () {
+const body = document.querySelector('body');
+class PolygonHelper {
+    static draw(x, y, npoints, radius) {
+        let angle = TWO_PI / npoints;
+        beginShape();
+        for (let a = 0; a < TWO_PI; a += angle) {
+            let sx = x + cos(a) * radius;
+            let sy = y + sin(a) * radius;
+            vertex(sx, sy);
+        }
+        endShape(CLOSE);
+    }
+}
+var sketch = (p) => {
+    const x = p.windowWidth / 2;
+    const y = p.windowHeight / 2;
+    p.setup = () => {
         p.createCanvas(p.windowWidth, p.windowHeight);
     };
-    p.draw = function () {
-        p.background(0);
-        p.fill(255);
-        p.rect(x, y, 50, 50);
+    p.draw = () => {
+        if (p.mouseIsPressed) {
+            p.fill(0);
+        }
+        else {
+            p.fill(255);
+        }
+        p.rect(p.mouseX, p.mouseY, 50, 50);
     };
 };
 new p5(sketch);
--- src/p5_1.ts
+++ src/p5_1.ts
@@ -1,6 +1,20 @@
 
 const body = document.querySelector('body')
 
+class PolygonHelper {
+  public static draw(x: number, y: number, npoints: number, radius: number) {
+    let angle: number = TWO_PI / npoints;
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += angle) {
+      let sx: number = x + cos(a) * radius;
+      let sy: number = y + sin(a) * radius;
+      vertex(sx, sy);
+    }
+    endShape(CLOSE);
+  }
+}
+
+
 var sketch = (p: p5) => {
   const x: number = p.windowWidth / 2;
   const y: number = p.windowHeight / 2;
@@ -9,9 +23,13 @@ var sketch = (p: p5) => {
   };
 
   p.draw = () => {
-    p.background(0);
-    p.fill(255);
-    p.rect(x, y, 50, 50);
+    if (p.mouseIsPressed) {
+      p.fill(0);
+    } else {
+      p.fill(255);
+    }
+    p.rect(p.mouseX, p.mouseY, 50, 50);
+
   };
 };
 
