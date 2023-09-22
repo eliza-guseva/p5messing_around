53fae2f25d6342944076e210e266729a2123ff7b
Elizaveta Guseva <elizaveta.guseva@glovoapp.com>
2023-09-22 21:05:11 +0200

    something is off with prims algorithm

--- src/labyrinth/graph.ts
+++ src/labyrinth/graph.ts
@@ -1,50 +1,150 @@
 
 
+export type PlaneCoord = [x: number, y:number];
+
+export function genWeight(start: PlaneCoord, end: PlaneCoord): number {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+
 export class Graph {
-    adjacencyList: Map<string, {node: string, weight: number}[]>;
+    adjacencyList: Map<string, 
+                    {node: string, weight: number}[]>;
 
     constructor() {
         this.adjacencyList = new Map();
     }
 
-    addVertex(vertex: string) {
-        if (!this.adjacencyList.has(vertex)) {
-            this.adjacencyList.set(vertex, []);
+    getVertexKey(vertex: PlaneCoord): string {
+        return vertex.toString();
+    }
+
+    StringToVertex(vertexStr: string): PlaneCoord {
+        return vertexStr.split(',').map(Number) as PlaneCoord;
+    }
+
+    addVertex(vertex: PlaneCoord) {
+        const vertexKey = this.getVertexKey(vertex);
+        if (!this.adjacencyList.has(vertexKey)) {
+            this.adjacencyList.set(vertexKey, []);
         }
     }
 
-    addEdge(vertex1: string, vertex2: string, weight: number) {
-        this.adjacencyList.get(vertex1).push({node: vertex2, weight: weight});
-        this.adjacencyList.get(vertex2).push({node: vertex1, weight: weight});
+    addEdge(vertex1: PlaneCoord, vertex2: PlaneCoord, weight: number) {
+        const vertex1Key = this.getVertexKey(vertex1);
+        const vertex2Key = this.getVertexKey(vertex2);
+        this.adjacencyList.get(vertex1Key).push({node: vertex2Key, weight: weight});
+        this.adjacencyList.get(vertex1Key).push({node: vertex2Key, weight: weight});
     }
-
-    generateSquareGrid(width: number, height: number) {
-        for (let iw = 0; iw < width; iw++) {
-            for (let ih = 0; ih < height; ih++) {
-                const vertex = `${iw},${ih}`;
-                this.addVertex(vertex);
-
-                // Connect with the left node
-                if (iw > 0) {
-                    const leftVertex = `${iw - 1},${ih}`;
-                    const weight = Math.floor(Math.random() * 100) + 1;
-                    this.addEdge(vertex, leftVertex, weight);
-                }
-                // Connect with the top node
-                if (ih > 0) {
-                    const topVertex = `${iw},${ih - 1}`;
-                    const weight = Math.floor(Math.random() * 100) + 1;
-                    this.addEdge(vertex, topVertex, weight);
-                }
-            }
-        };
-    }   
 
     printGraph(): void {
         for (let [vertex, neighbors] of this.adjacencyList) {
             const neighborsStr = neighbors.map(neighbor => `${neighbor.node}(${neighbor.weight})`).join(', ');
             console.log(`${vertex} -> ${neighborsStr}`);
         }
+    }
+}
+
+
+export class SquareGrid extends Graph {
+    center: PlaneCoord;
+    vertCoordinates: Map<string, PlaneCoord>;
+    x_step: number;
+    y_step: number;
+    x_offset: number;
+    y_offset: number;
+
+    constructor(width: number, height: number, p: p5) {
+        super();
+        this.center = [Math.ceil(width / 2), Math.ceil(height / 2)];
+        this.x_step = p.windowWidth / width;
+        this.y_step = p.windowHeight / height;
+        this.x_offset = this.x_step / 2;
+        this.y_offset = this.y_step / 2;
+        this.generateSquareGrid(width, height);
+        this.vertCoordinates = this.generateSquareDistances(
+            width, height);
+    }
+
+    generateSquareGrid(width: number, height: number) {
+        for (let iw = 0; iw < width; iw++) {
+            for (let ih = 0; ih < height; ih++) {
+                const vertex: PlaneCoord = [iw, ih];
+                this.addVertex(vertex);
+
+                // Connect with the left node
+                if (iw > 0) {
+                    const leftVertex: PlaneCoord = [iw - 1, ih];
+                    const weight = genWeight(vertex, leftVertex);
+                    this.addEdge(vertex, leftVertex, weight);
+                }
+                // Connect with the top node
+                if (ih > 0) {
+                    const topVertex: PlaneCoord = [iw, ih - 1];
+                    const weight = genWeight(vertex, topVertex);
+                    this.addEdge(vertex, topVertex, weight);
+                }
+            }
+        };
+    } 
+
+    generateSquareDistances(
+        width: number, // number of nodes in x direction
+        height: number, // number of nodes in y direction
+        ) {
+        // create empty map str -> PlaneCoord
+        let vertices_coords = new Map<string, PlaneCoord>();
+        for (let iw = 0; iw < width; iw++) {
+            for (let ih = 0; ih < height; ih++) {
+                vertices_coords.set(
+                    this.getVertexKey([iw, ih]), 
+                    [this.x_offset + iw * this.x_step, this.y_offset + ih * this.y_step]);
+        } 
+        }
+        return vertices_coords;
+    }
+
+    drawPrimsLabyrinth(p: p5) {
+        let startNode = this.getVertexKey(this.center);
+        let startCoords = this.vertCoordinates.get(startNode);
+        p.ellipse(startCoords[0], startCoords[1], this.x_step / 3, this.y_step / 3);
+        p.fill(255);
+
+        const nodes = this.adjacencyList.keys();
+        const visited: Set<string> = new Set();
+        const edgeList: { node: string, weight: number, from: string }[] = [];
+
+        visited.add(startNode);
+        let totalWeight = 0;
+
+        this.adjacencyList.get(startNode).forEach(edge => {
+            edgeList.push({ ...edge, from: startNode });
+        });
+
+        while (edgeList.length > 0) {
+            edgeList.sort((a, b) => a.weight - b.weight);
+            const nextEdge = edgeList.shift();
+
+            if (!visited.has(nextEdge.node)) {
+                visited.add(nextEdge.node);
+                p.ellipse(
+                    this.vertCoordinates.get(nextEdge.node)[0], 
+                    this.vertCoordinates.get(nextEdge.node)[1], 
+                    this.x_step / 3, 
+                    this.y_step / 3
+                );
+                p.fill(255);
+
+                totalWeight += nextEdge.weight;
+                console.log(`Edge added: ${nextEdge.from} - ${nextEdge.node}, weight: ${nextEdge.weight}`);
+                this.adjacencyList.get(nextEdge.node).forEach(edge => {
+                    if (!visited.has(edge.node)) {
+                        edgeList.push({ ...edge, from: nextEdge.node });
+                    }
+                });
+            }
+        }
+
     }
 }
 
--- src/labyrinth/graphics.ts
+++ src/labyrinth/graphics.ts
@@ -1,16 +1,24 @@
 
-import {Graph} from './graph';
+import {SquareGrid} from './graph';
 
 
-let graph = new Graph();
+
 
 
 var sketch = (p: p5) => {
+    let graph = new SquareGrid(29, Math.ceil(29 * p.windowHeight / p.windowWidth), p);
     p.setup = () => {
         p.createCanvas(p.windowWidth, p.windowHeight);
         p.background(0);
+        p.noLoop();
     };
-    p.draw = () => {};
+    p.draw = () => {
+        // for every coorinate pair in verticies_coords, draw a point
+        for (let [key, value] of graph.vertCoordinates) {
+            p.ellipse(value[0], value[1], 5, 5);
+            p.fill(100);
+            graph.drawPrimsLabyrinth(p);
+        }
+    }
 }
-
 new p5(sketch);
