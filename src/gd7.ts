edd6c61ab9656ff9c1e2a4c3e6025f4ce5bb5958
Elizaveta Guseva <liza.guseva@hey.com>
2024-01-06 19:34:09 +0100

    returning lost image

--- src/gd7.ts
+++ src/gd7.ts
@@ -1,19 +1,63 @@
 
+function drawInitImage(p: p5, img: p5.Image) {
+    p.background(0);
+    let destX = 0;
+    let destY = 0;
+    let destW = p.width;
+    let destH = p.height;
+    let srcX = 0;
+    let srcY = 0;
+    p.image(img, destX, destY, destW, destH, srcX, srcY, img.width, img.height);
+}
+
 var sketch = (p: p5) => {
     let img: p5.Image;
-    function preload() {
-        // preload image
-        img = p.loadImage('public/bull_head.png');
-    }
-
+    let colCount = 200;
+    let rowCount = 200;
+    let amp = 14;
+    p.preload = () =>
+        img = p.loadImage('public/munch.jpg'
+        );
 
     p.setup = () => {
-      p.createCanvas(p.windowWidth, p.windowHeight);
-      p.image(img, 0, 0, p.width, p.height);
+        p.createCanvas(p.windowHeight * img.width / img.height, p.windowHeight);
+        p.background(0);
+        drawInitImage(p, img);
     };
-  
     p.draw = () => {
+        //p.background(0);
+        for (let i=0; i<colCount; i++){
+            for (let j=0; j<rowCount; j++){
+                let nx = p.noise(
+                    i * 0.1, 
+                    j * 0.1, 
+                    p.frameCount * 0.02 + 500
+                ) * 2 - 1;
+                let ny = p.noise(
+                    i * 0.1, 
+                    j * 0.1, 
+                    p.frameCount * 0.02
+                    ) * 2 - 1;
+                let destW = p.width / colCount;
+                let destH = p.height / rowCount;
+                let destX = i * destW;
+                let destY = j * destH;
+                let srcW = img.width / colCount;
+                let srcH = img.height / rowCount;
+                let srcX = i * srcW + nx * Math.min(20, amp * p.frameCount * 0.01);
+                let srcY = j * srcH + ny * Math.min(20, amp * p.frameCount * 0.01);
+                //srcX = i * srcW + 30 * p.noise(p.frameCount * 0.01);
+                //srcY = j * srcH + 30 * p.noise(p.frameCount * 0.01);
+                // srcX = i * srcW + 10 * p.sin(p.frameCount * 0.01);
+                // srcY = j * srcH + 10 * p.cos(p.frameCount * 0.01);
+                // srcX = i * srcW + 10 * p.random() * p.sin(p.frameCount * 0.01);
+                // srcY = j * srcH + 10 * p.random() * p.cos(p.frameCount * 0.01);
+                p.image(img, 
+                    destX, destY, destW, destH, 
+                    srcX, srcY, srcW, srcH);
+            };
+        };
     };
-  };
-  
-  new p5(sketch);
+};
+new p5(sketch);
+//# sourceMappingURL=gd7.js.map
