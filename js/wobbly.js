90c5c1cac2cd0429da2e645e25ad5ce944931069
Elizaveta Guseva <liza.guseva@hey.com>
2024-01-13 18:19:16 +0100

    wobbly beginning

---
+++ js/wobbly.js
@@ -0,0 +1,35 @@
+function wobble1(x, t) {
+    return (t + 30 * Math.sin(0.02 * x + 30 + t / 100)
+        + 25 * Math.sin(0.03 * x + 2 + t / 130));
+}
+function genCurve(p, wobbleFunction, t = 0) {
+    p.beginShape();
+    for (let x = 0; x < p.windowWidth; x++) {
+        let y = wobbleFunction(x, t);
+        p.vertex(x, y);
+    }
+    p.vertex(p.windowWidth, p.windowHeight);
+    p.vertex(0, p.windowHeight);
+    p.endShape();
+}
+function frameCount2Y(p, t, how_far) {
+    return (p.windowHeight * 0.9
+        + how_far * p.windowHeight * (Math.sin(t / 100)
+            + Math.cos(2 + t / 130)));
+}
+var sketch = (p) => {
+    let scrW = p.windowWidth;
+    let scrH = p.windowHeight;
+    p.setup = () => {
+        p.createCanvas(p.windowWidth, p.windowHeight);
+        p.background(234, 228, 214);
+    };
+    p.draw = () => {
+        p.background(234, 228, 214);
+        p.noStroke();
+        p.fill(0, 105, 148);
+        genCurve(p, wobble1, frameCount2Y(p, p.frameCount, 0.25));
+    };
+};
+new p5(sketch);
+//# sourceMappingURL=wobbly.js.map
---
+++ src/wobbly.ts
@@ -0,0 +1,54 @@
+
+
+function wobble1(x: number, t: number) {
+    return (
+        t + 30* Math.sin(0.02 * x + 30 + t/100) 
+        + 25 * Math.sin( 0.03 * x + 2 + t/130)
+    )
+}
+
+function genCurve(p: p5, wobbleFunction: (x: number, t: number) => number, t: number = 0) {
+    p.beginShape();
+    for (let x = 0; x < p.windowWidth; x++) {
+        let y = wobbleFunction(x, t);
+        p.vertex(x, y);
+    }
+    p.vertex(p.windowWidth, p.windowHeight);
+    p.vertex(0, p.windowHeight);
+    p.endShape();
+}
+
+function frameCount2Y(p: p5, t: number, how_far: number) {
+    return (
+        p.windowHeight * 0.9 
+        + how_far * p.windowHeight * (
+            Math.sin(t / 100)
+            + Math.cos(2 + t / 130))
+    );
+}
+
+
+
+var sketch = (p: p5) => {
+    let scrW = p.windowWidth;
+    let scrH = p.windowHeight;
+
+    p.setup = () => {
+      p.createCanvas(p.windowWidth, p.windowHeight);
+      // fill with color of a dry sand
+      p.background(234,228,214);
+    };
+  
+    p.draw = () => {
+        p.background(234,228,214);
+        p.noStroke();
+        p.fill(0, 105, 148);
+        genCurve(p, wobble1, frameCount2Y(p, p.frameCount, 0.25));
+    }
+  };
+  
+  new p5(sketch);
+  
+  
+  
+  
